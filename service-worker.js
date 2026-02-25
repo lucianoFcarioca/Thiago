a58f91525e24fb95abf2cf9b65ef083b634f216e
Luciano Soares <cariocaluciano1805@gmail.com>
2026-02-25 15:12:49 -0300

    ajustes PWA

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,27 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("time-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "index.html",
-        "style.css",
-        "app.js",
-        "dados.json"
-      ]);
+const CACHE_NAME = "cec-cache-v1";
+
+const urlsToCache = [
+  "/Thiago/",
+  "/Thiago/index.html",
+  "/Thiago/style.css",
+  "/Thiago/app.js",
+  "/Thiago/dados.json",
+  "/Thiago/icon-192.png",
+  "/Thiago/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
